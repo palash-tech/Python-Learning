1f01f825c61b927979b71106b51ed50a4bb2e645
Palash <palashbhoyar6@gmail.com>
2026-07-04 21:05:06 +0530

    keyword arguments

---
+++ Functions/keyword_arguments.py
@@ -0,0 +1,7 @@
+def student(name,age,height):
+    print(name,age,height)
+student(age=19,name="Palash",height=5.9)
+
+def detail(name,college,city):
+    print(name,college,city)
+detail(college="SATI",city="Vidisha",name="Palash")
